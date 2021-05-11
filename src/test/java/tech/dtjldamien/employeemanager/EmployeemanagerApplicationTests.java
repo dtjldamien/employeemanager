61819b8c202260e07f9443ecabca6dc65e7d4a6e
Damien <dtjldamien@gmail.com>
2021-05-11 12:53:12 +0800

    feat: Employee entity

---
+++ src/test/java/tech/dtjldamien/employeemanager/EmployeemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dtjldamien.employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
