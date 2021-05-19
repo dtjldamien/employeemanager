d519d30709850fe4b00e88a52e0ffa48104da1a4
dtjldamien <dtjldamien@gmail.com>
2021-05-19 18:22:34 +0800

    fix: corsfilter and getemployees() in frontend

--- angular-app/src/app/employee.service.ts
+++ angular-app/src/app/employee.service.ts
@@ -2,17 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Employee } from './employee';
-
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
-  private apiServiceUrl = '';
+  private apiServiceUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) {}
 
-  public getEmployees(): Observable<Employee> {
-    return this.http.get<Employee>(`${this.apiServiceUrl}/employee/all`);
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServiceUrl}/employee/all`);
   }
 
   public addEmployee(employee: Employee): Observable<Employee> {
